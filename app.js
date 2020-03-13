a0b5ce51ea77e33b96fb31b9f0843669bcd2b34b
simon caceres <kcrs2194@gmail.com>
2020-03-13 15:49:17 -0300

    desarrollo de contandores y estilos

---
+++ app.js
@@ -0,0 +1,79 @@
+var intervalTimer;
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            mensaje : ' Contador!',
+            selectedTime: 0 ,
+            timeLeft: '00.00',
+            endTime: '0',
+            times: [
+                {
+                     sec: 3,
+                     display: '3s'
+                },
+                {
+                    sec: 600,
+                    display: '10m'
+                },
+                {
+                    sec: 1800,
+                    display: '30m'
+                }
+            ]
+        }
+    },
+
+    methods: {
+        setTime(seconds){
+            clearInterval(intervalTimer);
+            this.timer(seconds);
+        },
+        timer(seconds){
+            const now = Date.now();
+            const end = now + seconds * 1000;
+            this.displayTimeLeft(seconds);
+
+            this.selectedTime = seconds;
+
+            this.displayEndTime(end);
+            this.countdown(end);
+        },
+        countdown(end){
+            intervalTimer = setInterval(()=>{
+                const secondsLeft = Math.round((end - Date.now()) / 1000);
+
+                if(secondsLeft === 0 ){
+                    this.endTime = 0;
+                }
+                else if(secondsLeft < 0) {
+                    clearInterval(intervalTimer);
+                    return;
+                }
+                this.displayTimeLeft(secondsLeft)
+            }, 1000);
+        },
+        displayTimeLeft(secondsLeft){
+            const minutes = Math.floor((secondsLeft % 3600) / 60 );
+            const seconds = secondsLeft % 60;
+
+            this.timeLeft = `${zeroPadded(minutes)}:${zeroPadded(seconds)}`;
+        },
+        displayEndTime(timestamp){
+            const end = new Date(timestamp);
+            const hour = end.getHours();
+            const minutes = end.getMinutes();
+
+            this.endTime = `${hourConvert(hour)}:${zeroPadded(minutes)}`
+        },
+    },
+});
+
+function zeroPadded(num){
+    return num < 10 ? `0${num}` : num;
+}
+
+function hourConvert(hour){
+    return (hour % 12) || 12;
+}
+
